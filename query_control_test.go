337e7067b5c22bbd36a3eaf9445c9fa0fc54b65d
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-12 18:22:02 +0100

    Added some more tests for Expr/Do

--- query_control_test.go
+++ query_control_test.go
@@ -6,10 +6,42 @@ import (
 )
 
 func (s *RethinkSuite) TestControlExecList(c *test.C) {
-	tree := Expr(List{
+	query := Expr(List{
 		1, 2, 3, 4, 5, 6, List{
 			7.1, 7.2, 7.3,
 		},
 	})
-	fmt.Println(tree.String())
+	fmt.Println(query.String())
+}
+
+// func (s *RethinkSuite) TestControlExecObj(c *test.C) {
+// 	query := Expr(Obj{
+// 		"A": 1,
+// 		"B": 2,
+// 		"C": Obj{
+// 			"1": 3,
+// 			"2": 4,
+// 		},
+// 	})
+// 	fmt.Println(query.String())
+// }
+
+func (s *RethinkSuite) TestControlDo(c *test.C) {
+	query := Do(Obj{"a": 1}, Obj{"a": 2}, Obj{"a": 3}, func(row RqlTerm) RqlTerm {
+		// return row.Field("a")
+		return row
+	})
+	fmt.Println(query.String())
+}
+
+func (s *RethinkSuite) TestControlDoWithExpr(c *test.C) {
+	query := Expr(List{
+		Obj{"a": 1},
+		Obj{"a": 2},
+		Obj{"a": 3},
+	}).Do(func(row RqlTerm) RqlTerm {
+		// return row.Field("a")
+		return row
+	})
+	fmt.Println(query.String())
 }
