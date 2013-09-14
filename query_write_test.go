bff6c2ddd31c43c3b44f8ccead4d369d7c6d8ad2
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-14 13:30:50 +0100

    Added some tests for the write functions. They do not pass yet as they rely on the Nth function

--- query_write_test.go
+++ query_write_test.go
@@ -5,8 +5,40 @@ import (
 )
 
 func (s *RethinkSuite) TestWriteInsert(c *test.C) {
-	// Insert 1500 test rows
 	query := Db("test").Table("test").Insert(Obj{"num": 1})
 	_, err := query.Run(conn)
+	c.Assert(err, test.IsNil)
+}
+
+func (s *RethinkSuite) TestWriteUpdate(c *test.C) {
+	query := Db("test").Table("test").Insert(Obj{"num": 1})
+	_, err := query.Run(conn)
+	c.Assert(err, test.IsNil)
+
+	// Update the first row in the table
+	query = Db("test").Table("test").Nth(1).Update(Obj{"num": 2})
+	_, err = query.Run(conn)
+	c.Assert(err, test.IsNil)
+}
+
+func (s *RethinkSuite) TestWriteReplacec(c *test.C) {
+	query := Db("test").Table("test").Insert(Obj{"num": 1})
+	_, err := query.Run(conn)
+	c.Assert(err, test.IsNil)
+
+	// Replace the first row in the table
+	query = Db("test").Table("test").Nth(1).Update(Obj{"num": 2})
+	_, err = query.Run(conn)
+	c.Assert(err, test.IsNil)
+}
+
+func (s *RethinkSuite) TestWriteDelete(c *test.C) {
+	query := Db("test").Table("test").Insert(Obj{"num": 1})
+	_, err := query.Run(conn)
+	c.Assert(err, test.IsNil)
+
+	// Delete the first row in the table
+	query = Db("test").Table("test").Nth(1).Delete()
+	_, err = query.Run(conn)
 	c.Assert(err, test.IsNil)
 }
