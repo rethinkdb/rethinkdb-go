c1005142e9ac9c05259867090e91c44cc9a749e1
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-14 14:05:54 +0100

    Added the rest of the selection ReQL functions

--- query_select.go
+++ query_select.go
@@ -11,3 +11,19 @@ func Db(name string) RqlTerm {
 func (t RqlTerm) Table(name string) RqlTerm {
 	return newRqlTermFromPrevVal(t, "Table", p.Term_TABLE, List{name}, Obj{})
 }
+
+func (t RqlTerm) Get(key interface{}) RqlTerm {
+	return newRqlTermFromPrevVal(t, "Get", p.Term_GET, List{key}, Obj{})
+}
+
+func (t RqlTerm) GetAll(keys ...interface{}) RqlTerm {
+	return newRqlTermFromPrevVal(t, "GetAll", p.Term_GET_ALL, keys, Obj{})
+}
+
+func (t RqlTerm) Between(lowerKey, upperKey interface{}) RqlTerm {
+	return newRqlTermFromPrevVal(t, "Table", p.Term_TABLE, List{lowerKey, upperKey}, Obj{})
+}
+
+func (t RqlTerm) Filter(arg interface{}) RqlTerm {
+	return newRqlTermFromPrevVal(t, "Table", p.Term_TABLE, List{arg}, Obj{})
+}
