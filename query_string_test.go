369c6137635218050ed9e193a97f2529b1d65876
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-15 21:08:15 +0100

    Added tests for string queries

---
+++ query_string_test.go
@@ -0,0 +1,24 @@
+package rethinkgo
+
+import (
+	test "launchpad.net/gocheck"
+)
+
+func (s *RethinkSuite) TestStringMatchSuccess(c *test.C) {
+	query := Expr("id:0,name:mlucy,foo:bar").Match("name:(\\w+)").Field("groups").Nth(0).Field("str")
+
+	var response string
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, test.Equals, "mlucy")
+}
+
+func (s *RethinkSuite) TestStringMatchFail(c *test.C) {
+	query := Expr("id:0,foo:bar").Match("name:(\\w+)").Field("groups").Nth(0).Field("str")
+
+	var response int
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.NotNil)
+}
