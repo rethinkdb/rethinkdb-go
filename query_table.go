41f041d4cebfd248b2cbd003465e6ca76114f7c9
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-16 15:57:53 +0100

    Added optional arguments for the table functions

--- query_table.go
+++ query_table.go
@@ -4,8 +4,9 @@ import (
 	p "github.com/dancannon/gorethink/ql2"
 )
 
-func (t RqlTerm) TableCreate(name string) RqlTerm {
-	return newRqlTermFromPrevVal(t, "TableCreate", p.Term_TABLE_CREATE, List{name}, Obj{})
+func (t RqlTerm) TableCreate(name string, optArgs ...interface{}) RqlTerm {
+	optArgM := optArgsToMap([]string{"primary_key", "durability", "cache_size", "datacenter"}, optArgs)
+	return newRqlTermFromPrevVal(t, "TableCreate", p.Term_TABLE_CREATE, List{name}, optArgM)
 }
 
 func (t RqlTerm) TableDrop(name string) RqlTerm {
