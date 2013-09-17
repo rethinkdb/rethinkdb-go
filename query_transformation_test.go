e14b7968da4e4f6fcbfd076d807931788bb0abde
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-17 21:12:51 +0100

    Added tests for transformation ReQL functions

--- query_transformation_test.go
+++ query_transformation_test.go
@@ -1,1 +1,211 @@
 package rethinkgo
+
+import (
+	test "launchpad.net/gocheck"
+)
+
+func (s *RethinkSuite) TestTransformationMapImplicit(c *test.C) {
+	query := Expr(arr).Map(Doc().Add(1))
+
+	var response []interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{2, 3, 4, 5, 6, 7, 8, 9, 10})
+}
+
+func (s *RethinkSuite) TestTransformationMapFunc(c *test.C) {
+	query := Expr(arr).Map(func(row RqlTerm) RqlTerm {
+		return row.Add(1)
+	})
+
+	var response []interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{2, 3, 4, 5, 6, 7, 8, 9, 10})
+}
+
+func (s *RethinkSuite) TestTransformationWithFields(c *test.C) {
+	query := Expr(objList).WithFields("id", "num").OrderBy("id")
+
+	var response []interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{
+		map[string]interface{}{"num": 0, "id": 1},
+		map[string]interface{}{"num": 5, "id": 2},
+		map[string]interface{}{"num": 10, "id": 3},
+		map[string]interface{}{"num": 0, "id": 4},
+		map[string]interface{}{"num": 100, "id": 5},
+		map[string]interface{}{"num": 15, "id": 6},
+		map[string]interface{}{"num": 0, "id": 7},
+		map[string]interface{}{"num": 50, "id": 8},
+		map[string]interface{}{"num": 25, "id": 9},
+	})
+}
+
+func (s *RethinkSuite) TestTransformationConcatMap(c *test.C) {
+	query := Expr(objList).ConcatMap(func(row RqlTerm) RqlTerm {
+		return Expr([]interface{}{row.Field("num")})
+	})
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{0, 5, 10, 0, 100, 15, 0, 50, 25})
+}
+
+func (s *RethinkSuite) TestTransformationOrderByDesc(c *test.C) {
+	query := Expr(objList).OrderBy(Desc("num"))
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{
+		map[string]interface{}{"num": 100, "id": 5, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 50, "id": 8, "g2": 2, "g1": 4},
+		map[string]interface{}{"num": 25, "id": 9, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 15, "id": 6, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 10, "id": 3, "g2": 2, "g1": 3},
+		map[string]interface{}{"num": 5, "id": 2, "g2": 2, "g1": 2},
+		map[string]interface{}{"num": 0, "id": 1, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 0, "id": 4, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 0, "id": 7, "g2": 2, "g1": 1},
+	})
+}
+
+func (s *RethinkSuite) TestTransformationOrderByAsc(c *test.C) {
+	query := Expr(objList).OrderBy(Asc("num"))
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{
+		map[string]interface{}{"num": 0, "id": 1, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 0, "id": 4, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 0, "id": 7, "g2": 2, "g1": 1},
+		map[string]interface{}{"num": 5, "id": 2, "g2": 2, "g1": 2},
+		map[string]interface{}{"num": 10, "id": 3, "g2": 2, "g1": 3},
+		map[string]interface{}{"num": 15, "id": 6, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 25, "id": 9, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 50, "id": 8, "g2": 2, "g1": 4},
+		map[string]interface{}{"num": 100, "id": 5, "g2": 3, "g1": 2},
+	})
+}
+
+func (s *RethinkSuite) TestTransformationOrderByMultiple(c *test.C) {
+	query := Expr(objList).OrderBy(Desc("num"), Asc("id"))
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{
+		map[string]interface{}{"num": 100, "id": 5, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 50, "id": 8, "g2": 2, "g1": 4},
+		map[string]interface{}{"num": 25, "id": 9, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 15, "id": 6, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 10, "id": 3, "g2": 2, "g1": 3},
+		map[string]interface{}{"num": 5, "id": 2, "g2": 2, "g1": 2},
+		map[string]interface{}{"num": 0, "id": 1, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 0, "id": 4, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 0, "id": 7, "g2": 2, "g1": 1},
+	})
+}
+
+func (s *RethinkSuite) TestTransformationOrderByFunc(c *test.C) {
+	query := Expr(objList).OrderBy(func(row RqlTerm) RqlTerm {
+		return row.Field("num").Add(row.Field("id"))
+	})
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{
+		map[string]interface{}{"num": 0, "id": 1, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 0, "id": 4, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 5, "id": 2, "g2": 2, "g1": 2},
+		map[string]interface{}{"num": 0, "id": 7, "g2": 2, "g1": 1},
+		map[string]interface{}{"num": 10, "id": 3, "g2": 2, "g1": 3},
+		map[string]interface{}{"num": 15, "id": 6, "g2": 1, "g1": 1},
+		map[string]interface{}{"num": 25, "id": 9, "g2": 3, "g1": 2},
+		map[string]interface{}{"num": 50, "id": 8, "g2": 2, "g1": 4},
+		map[string]interface{}{"num": 100, "id": 5, "g2": 3, "g1": 2},
+	})
+}
+
+func (s *RethinkSuite) TestTransformationSkip(c *test.C) {
+	query := Expr(arr).Skip(7)
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{8, 9})
+}
+
+func (s *RethinkSuite) TestTransformationLimit(c *test.C) {
+	query := Expr(arr).Limit(2)
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{1, 2})
+}
+
+func (s *RethinkSuite) TestTransformationSlice(c *test.C) {
+	query := Expr(arr).Slice(5, 6)
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{6})
+}
+
+func (s *RethinkSuite) TestTransformationNth(c *test.C) {
+	query := Expr(arr).Nth(2)
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, 3)
+}
+
+func (s *RethinkSuite) TestTransformationIndexesOf(c *test.C) {
+	query := Expr(arr).IndexesOf(2)
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{1})
+}
+
+func (s *RethinkSuite) TestTransformationIsEmpty(c *test.C) {
+	query := Expr([]interface{}{}).IsEmpty()
+
+	var response bool
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, test.Equals, true)
+}
+
+func (s *RethinkSuite) TestTransformationUnion(c *test.C) {
+	query := Expr(arr).Union(arr)
+
+	var response interface{}
+	err := query.RunRow(conn).Scan(&response)
+
+	c.Assert(err, test.IsNil)
+	c.Assert(response, JsonEquals, []interface{}{1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+}
