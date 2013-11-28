803c6803c1c47a864ebb6764bd57d95dfa52a041
Daniel Cannon <daniel@danielcannon.co.uk>
2013-11-28 19:59:08 +0000

    Updated version in GoDoc, fixes #50

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Go driver for RethinkDB
 //
-// Current supported RethinkDB version: 1.9
+// Current supported RethinkDB version: 1.11
 // Documentation: [GoDoc](http://godoc.org/github.com/dancannon/gorethink)
 // For more in depth information on how to use RethinkDB check out the API docs
 // at http://rethinkdb.com/api
