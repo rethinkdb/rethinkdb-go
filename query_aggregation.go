291ba762dec262348ccd89a3499402a12391c182
Daniel Cannon <daniel@danielcannon.co.uk>
2014-04-11 20:19:36 +0100

    Removed GroupBy and GroupedMapReduce

--- query_aggregation.go
+++ query_aggregation.go
@@ -34,27 +34,6 @@ func (t RqlTerm) Distinct() RqlTerm {
 	return newRqlTermFromPrevVal(t, "Distinct", p.Term_DISTINCT, []interface{}{}, map[string]interface{}{})
 }
 
-// Partition the sequence into groups based on the grouping function. The elements
-// of each group are then mapped using the mapping function and reduced using the
-// reduction function.
-func (t RqlTerm) GroupedMapReduce(grouping, mapping, reduction, base interface{}) RqlTerm {
-	return newRqlTermFromPrevVal(
-		t,
-		"GroupedMapReduce",
-		p.Term_GROUPED_MAP_REDUCE,
-		[]interface{}{funcWrap(grouping), funcWrap(mapping), funcWrap(reduction)},
-		map[string]interface{}{"base": base},
-	)
-}
-
-// Groups elements by the values of the given attributes and then applies the given
-// reduction. Though similar to GroupedMapReduce, GroupBy takes a standardized
-// object for specifying the reduction. Can be used with a number of predefined
-// common reductions
-func (t RqlTerm) GroupBy(collector interface{}, args ...interface{}) RqlTerm {
-	return newRqlTermFromPrevVal(t, "GroupBy", p.Term_GROUPBY, []interface{}{args, collector}, map[string]interface{}{})
-}
-
 //Returns whether or not a sequence contains all the specified values, or if
 //functions are provided instead, returns whether or not a sequence contains
 //values matching all the specified functions.
