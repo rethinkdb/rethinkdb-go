d7a24636e53f5d638bbbb6c6d99831acab6c1d1e
Daniel Cannon <daniel@danielcannon.co.uk>
2014-06-25 14:23:47 +0100

    Added random term

--- query_math.go
+++ query_math.go
@@ -196,3 +196,36 @@ func Not(args ...interface{}) RqlTerm {
 
 	return newRqlTerm("Not", p.Term_NOT, args, map[string]interface{}{})
 }
+
+type RandomOpts struct {
+	Float interface{} `gorethink:"float,omitempty"`
+}
+
+func (o *RandomOpts) toMap() map[string]interface{} {
+	return optArgsToMap(o)
+}
+
+// Generate a random number between the given bounds. If no arguments are
+// given, the result will be a floating-point number in the range [0,1).
+//
+// When passing a single argument, r.random(x), the result will be in the
+// range [0,x), and when passing two arguments, r.random(x,y), the range is
+// [x,y). If x and y are equal, an error will occur, unless generating a
+// floating-point number, for which x will be returned.
+//
+// Note: The last argument given will always be the 'open' side of the range,
+// but when generating a floating-point number, the 'open' side may be less
+// than the 'closed' side.
+func (t RqlTerm) Random(args ...interface{}) RqlTerm {
+	var opts = map[string]interface{}{}
+
+	// Look for options map
+	if len(args) > 0 {
+		if possibleOpts, ok := args[len(args)-1].(RandomOpts); ok {
+			opts = possibleOpts
+			args = args[:len(args)-1]
+		}
+	}
+
+	return newRqlTermFromPrevVal(t, "Random", p.Term_RANDOM, args, opts)
+}
