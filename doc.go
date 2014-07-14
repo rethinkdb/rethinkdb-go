c90be4d93db02bca76d234de91f5c4f3d6cc9936
Daniel Cannon <daniel@danielcannon.co.uk>
2014-07-14 17:14:51 +0100

    Bumped version number and updated change log

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Go driver for RethinkDB
 //
-// Current version: v0.3 (RethinkDB v1.13)
+// Current version: v0.3.1 (RethinkDB v1.13)
 // For more in depth information on how to use RethinkDB check out the API docs
 // at http://rethinkdb.com/api
 package gorethink
