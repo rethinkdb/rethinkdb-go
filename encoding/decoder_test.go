cda356629eac81c3d88a2c1007988c46fc120e47
Daniel Cannon <daniel@danielcannon.co.uk>
2014-12-29 21:43:46 +0000

    Uncommented decoder tests

--- encoding/decoder_test.go
+++ encoding/decoder_test.go
@@ -140,105 +140,105 @@ type Ambig struct {
 
 var decodeTests = []decodeTest{
 	// basic types
-	// {in: true, ptr: new(bool), out: true},
-	// {in: 1, ptr: new(int), out: 1},
-	// {in: 1.2, ptr: new(float64), out: 1.2},
-	// {in: -5, ptr: new(int16), out: int16(-5)},
-	// {in: 2, ptr: new(string), out: string("2")},
-	// {in: float64(2.0), ptr: new(interface{}), out: float64(2.0)},
-	// {in: string("2"), ptr: new(interface{}), out: string("2")},
-	// {in: "a\u1234", ptr: new(string), out: "a\u1234"},
-	// {in: []interface{}{}, ptr: new([]string), out: []string{}},
-	// {in: map[string]interface{}{"X": []interface{}{1, 2, 3}, "Y": 4}, ptr: new(T), out: T{}, err: &DecodeTypeError{reflect.TypeOf([0]interface{}{}), reflect.TypeOf(""), ""}},
-	// {in: map[string]interface{}{"x": 1}, ptr: new(tx), out: tx{}},
-	// {in: map[string]interface{}{"F1": float64(1), "F2": 2, "F3": 3}, ptr: new(V), out: V{F1: float64(1), F2: int32(2), F3: string("3")}},
-	// {in: map[string]interface{}{"F1": string("1"), "F2": 2, "F3": 3}, ptr: new(V), out: V{F1: string("1"), F2: int32(2), F3: string("3")}},
-	// {
-	// 	in:  map[string]interface{}{"k1": int64(1), "k2": "s", "k3": []interface{}{int64(1), 2.0, 3e-3}, "k4": map[string]interface{}{"kk1": "s", "kk2": int64(2)}},
-	// 	out: map[string]interface{}{"k1": int64(1), "k2": "s", "k3": []interface{}{int64(1), 2.0, 3e-3}, "k4": map[string]interface{}{"kk1": "s", "kk2": int64(2)}},
-	// 	ptr: new(interface{}),
-	// },
+	{in: true, ptr: new(bool), out: true},
+	{in: 1, ptr: new(int), out: 1},
+	{in: 1.2, ptr: new(float64), out: 1.2},
+	{in: -5, ptr: new(int16), out: int16(-5)},
+	{in: 2, ptr: new(string), out: string("2")},
+	{in: float64(2.0), ptr: new(interface{}), out: float64(2.0)},
+	{in: string("2"), ptr: new(interface{}), out: string("2")},
+	{in: "a\u1234", ptr: new(string), out: "a\u1234"},
+	{in: []interface{}{}, ptr: new([]string), out: []string{}},
+	{in: map[string]interface{}{"X": []interface{}{1, 2, 3}, "Y": 4}, ptr: new(T), out: T{}, err: &DecodeTypeError{reflect.TypeOf([0]interface{}{}), reflect.TypeOf(""), ""}},
+	{in: map[string]interface{}{"x": 1}, ptr: new(tx), out: tx{}},
+	{in: map[string]interface{}{"F1": float64(1), "F2": 2, "F3": 3}, ptr: new(V), out: V{F1: float64(1), F2: int32(2), F3: string("3")}},
+	{in: map[string]interface{}{"F1": string("1"), "F2": 2, "F3": 3}, ptr: new(V), out: V{F1: string("1"), F2: int32(2), F3: string("3")}},
+	{
+		in:  map[string]interface{}{"k1": int64(1), "k2": "s", "k3": []interface{}{int64(1), 2.0, 3e-3}, "k4": map[string]interface{}{"kk1": "s", "kk2": int64(2)}},
+		out: map[string]interface{}{"k1": int64(1), "k2": "s", "k3": []interface{}{int64(1), 2.0, 3e-3}, "k4": map[string]interface{}{"kk1": "s", "kk2": int64(2)}},
+		ptr: new(interface{}),
+	},
 
-	// // Z has a "-" tag.
-	// {in: map[string]interface{}{"Y": 1, "Z": 2}, ptr: new(T), out: T{Y: 1}},
+	// Z has a "-" tag.
+	{in: map[string]interface{}{"Y": 1, "Z": 2}, ptr: new(T), out: T{Y: 1}},
 
-	// {in: map[string]interface{}{"alpha": "abc", "alphabet": "xyz"}, ptr: new(U), out: U{Alphabet: "abc"}},
-	// {in: map[string]interface{}{"alpha": "abc"}, ptr: new(U), out: U{Alphabet: "abc"}},
-	// {in: map[string]interface{}{"alphabet": "xyz"}, ptr: new(U), out: U{}},
+	{in: map[string]interface{}{"alpha": "abc", "alphabet": "xyz"}, ptr: new(U), out: U{Alphabet: "abc"}},
+	{in: map[string]interface{}{"alpha": "abc"}, ptr: new(U), out: U{Alphabet: "abc"}},
+	{in: map[string]interface{}{"alphabet": "xyz"}, ptr: new(U), out: U{}},
 
-	// // array tests
-	// {in: []interface{}{1, 2, 3}, ptr: new([3]int), out: [3]int{1, 2, 3}},
-	// {in: []interface{}{1, 2, 3}, ptr: new([1]int), out: [1]int{1}},
-	// {in: []interface{}{1, 2, 3}, ptr: new([5]int), out: [5]int{1, 2, 3, 0, 0}},
+	// array tests
+	{in: []interface{}{1, 2, 3}, ptr: new([3]int), out: [3]int{1, 2, 3}},
+	{in: []interface{}{1, 2, 3}, ptr: new([1]int), out: [1]int{1}},
+	{in: []interface{}{1, 2, 3}, ptr: new([5]int), out: [5]int{1, 2, 3, 0, 0}},
 
-	// // empty array to interface test
-	// {in: map[string]interface{}{"T": []interface{}{}}, ptr: new(map[string]interface{}), out: map[string]interface{}{"T": []interface{}{}}},
+	// empty array to interface test
+	{in: map[string]interface{}{"T": []interface{}{}}, ptr: new(map[string]interface{}), out: map[string]interface{}{"T": []interface{}{}}},
 
-	// {
-	// 	in: map[string]interface{}{
-	// 		"Level0":  1,
-	// 		"Level1b": 2,
-	// 		"Level1c": 3,
-	// 		"level1d": 4,
-	// 		"Level1a": 5,
-	// 		"LEVEL1B": 6,
-	// 		"e": map[string]interface{}{
-	// 			"Level1a": 8,
-	// 			"Level1b": 9,
-	// 			"Level1c": 10,
-	// 			"Level1d": 11,
-	// 			"x":       12,
-	// 		},
-	// 		"Loop1": 13,
-	// 		"Loop2": 14,
-	// 		"X":     15,
-	// 		"Y":     16,
-	// 		"Z":     17,
-	// 	},
-	// 	ptr: new(Top),
-	// 	out: Top{
-	// 		Level0: 1,
-	// 		Embed0: Embed0{
-	// 			Level1b: 2,
-	// 			Level1c: 3,
-	// 		},
-	// 		Embed0a: &Embed0a{
-	// 			Level1a: 5,
-	// 			Level1b: 6,
-	// 		},
-	// 		Embed0b: &Embed0b{
-	// 			Level1a: 8,
-	// 			Level1b: 9,
-	// 			Level1c: 10,
-	// 			Level1d: 11,
-	// 		},
-	// 		Loop: Loop{
-	// 			Loop1: 13,
-	// 			Loop2: 14,
-	// 		},
-	// 		Embed0p: Embed0p{
-	// 			Point: image.Point{X: 15, Y: 16},
-	// 		},
-	// 		Embed0q: Embed0q{
-	// 			Point: Point{Z: 17},
-	// 		},
-	// 	},
-	// },
+	{
+		in: map[string]interface{}{
+			"Level0":  1,
+			"Level1b": 2,
+			"Level1c": 3,
+			"level1d": 4,
+			"Level1a": 5,
+			"LEVEL1B": 6,
+			"e": map[string]interface{}{
+				"Level1a": 8,
+				"Level1b": 9,
+				"Level1c": 10,
+				"Level1d": 11,
+				"x":       12,
+			},
+			"Loop1": 13,
+			"Loop2": 14,
+			"X":     15,
+			"Y":     16,
+			"Z":     17,
+		},
+		ptr: new(Top),
+		out: Top{
+			Level0: 1,
+			Embed0: Embed0{
+				Level1b: 2,
+				Level1c: 3,
+			},
+			Embed0a: &Embed0a{
+				Level1a: 5,
+				Level1b: 6,
+			},
+			Embed0b: &Embed0b{
+				Level1a: 8,
+				Level1b: 9,
+				Level1c: 10,
+				Level1d: 11,
+			},
+			Loop: Loop{
+				Loop1: 13,
+				Loop2: 14,
+			},
+			Embed0p: Embed0p{
+				Point: image.Point{X: 15, Y: 16},
+			},
+			Embed0q: Embed0q{
+				Point: Point{Z: 17},
+			},
+		},
+	},
 	{
 		in:  map[string]interface{}{"hello": 1},
 		ptr: new(Ambig),
 		out: Ambig{First: 1},
 	},
-	// {
-	// 	in:  map[string]interface{}{"X": 1, "Y": 2},
-	// 	ptr: new(S5),
-	// 	out: S5{S8: S8{S9: S9{Y: 2}}},
-	// },
-	// {
-	// 	in:  map[string]interface{}{"X": 1, "Y": 2},
-	// 	ptr: new(S10),
-	// 	out: S10{S13: S13{S8: S8{S9: S9{Y: 2}}}},
-	// },
+	{
+		in:  map[string]interface{}{"X": 1, "Y": 2},
+		ptr: new(S5),
+		out: S5{S8: S8{S9: S9{Y: 2}}},
+	},
+	{
+		in:  map[string]interface{}{"X": 1, "Y": 2},
+		ptr: new(S10),
+		out: S10{S13: S13{S8: S8{S9: S9{Y: 2}}}},
+	},
 }
 
 func TestDecode(t *testing.T) {
