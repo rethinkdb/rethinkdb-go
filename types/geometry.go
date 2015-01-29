7360752974898f391564f25ca734ef3bb308cae6
Daniel Cannon <daniel@danielcannon.co.uk>
2015-01-29 00:26:23 +0000

    Updated geometry types to use the Marshaler/Unmarshaler interfaces

--- types/geometry.go
+++ types/geometry.go
@@ -9,6 +9,56 @@ type Geometry struct {
 	Lines Lines
 }
 
+func (g Geometry) MarshalRQL() (interface{}, error) {
+	switch g.Type {
+	case "Point":
+		return g.Point.MarshalRQL()
+	case "LineString":
+		return g.Line.MarshalRQL()
+	case "Polygon":
+		return g.Lines.MarshalRQL()
+	default:
+		return nil, fmt.Errorf("pseudo-type GEOMETRY object field 'type' %s is not valid", g.Type)
+	}
+}
+
+func (g *Geometry) UnmarshalRQL(data interface{}) error {
+	m, ok := data.(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("pseudo-type GEOMETRY object is not valid")
+	}
+
+	typ, ok := m["type"]
+	if !ok {
+		return fmt.Errorf("pseudo-type GEOMETRY object is not valid, expects 'type' field")
+	}
+	coords, ok := m["coordinates"]
+	if !ok {
+		return fmt.Errorf("pseudo-type GEOMETRY object is not valid, expects 'coordinates' field")
+	}
+
+	var err error
+	switch typ {
+	case "Point":
+		g.Type = "Point"
+		g.Point, err = UnmarshalPoint(coords)
+	case "LineString":
+		g.Type = "LineString"
+		g.Line, err = UnmarshalLineString(coords)
+	case "Polygon":
+		g.Type = "Polygon"
+		g.Lines, err = UnmarshalPolygon(coords)
+	default:
+		return fmt.Errorf("pseudo-type GEOMETRY object has invalid type")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 type Point struct {
 	Lon float64
 	Lat float64
@@ -20,12 +70,28 @@ func (p Point) Coords() interface{} {
 	return []interface{}{p.Lon, p.Lat}
 }
 
-func (p Point) Marshal() interface{} {
+func (p Point) MarshalRQL() (interface{}, error) {
 	return map[string]interface{}{
 		"$reql_type$": "GEOMETRY",
 		"coordinates": p.Coords(),
 		"type":        "Point",
+	}, nil
+}
+
+func (p *Point) UnmarshalRQL(data interface{}) error {
+	g := &Geometry{}
+	err := g.UnmarshalRQL(data)
+	if err != nil {
+		return err
 	}
+	if g.Type != "Point" {
+		return fmt.Errorf("pseudo-type GEOMETRY object has type %s, expected type %s", g.Type, "Point")
+	}
+
+	p.Lat = g.Point.Lat
+	p.Lon = g.Point.Lon
+
+	return nil
 }
 
 func (l Line) Coords() interface{} {
@@ -36,12 +102,27 @@ func (l Line) Coords() interface{} {
 	return coords
 }
 
-func (l Line) Marshal() interface{} {
+func (l Line) MarshalRQL() (interface{}, error) {
 	return map[string]interface{}{
 		"$reql_type$": "GEOMETRY",
 		"coordinates": l.Coords(),
-		"type":        "Line",
+		"type":        "LineString",
+	}, nil
+}
+
+func (l *Line) UnmarshalRQL(data interface{}) error {
+	g := &Geometry{}
+	err := g.UnmarshalRQL(data)
+	if err != nil {
+		return err
 	}
+	if g.Type != "LineString" {
+		return fmt.Errorf("pseudo-type GEOMETRY object has type %s, expected type %s", g.Type, "LineString")
+	}
+
+	*l = g.Line
+
+	return nil
 }
 
 func (l Lines) Coords() interface{} {
@@ -52,29 +133,44 @@ func (l Lines) Coords() interface{} {
 	return coords
 }
 
-func (l Lines) Marshal() interface{} {
+func (l Lines) MarshalRQL() (interface{}, error) {
 	return map[string]interface{}{
 		"$reql_type$": "GEOMETRY",
 		"coordinates": l.Coords(),
-		"type":        "Lines",
+		"type":        "Polygon",
+	}, nil
+}
+
+func (l *Lines) UnmarshalRQL(data interface{}) error {
+	g := &Geometry{}
+	err := g.UnmarshalRQL(data)
+	if err != nil {
+		return err
 	}
+	if g.Type != "Polygon" {
+		return fmt.Errorf("pseudo-type GEOMETRY object has type %s, expected type %s", g.Type, "Polygon")
+	}
+
+	*l = g.Lines
+
+	return nil
 }
 
 func UnmarshalPoint(v interface{}) (Point, error) {
 	coords, ok := v.([]interface{})
 	if !ok {
-		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object %v field \"coordinates\" is not valid", v)
+		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object field 'coordinates' is not valid")
 	}
 	if len(coords) != 2 {
-		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object %v field \"coordinates\" is not valid", v)
+		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object field 'coordinates' is not valid")
 	}
 	lon, ok := coords[0].(float64)
 	if !ok {
-		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object %v field \"coordinates\" is not valid", v)
+		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object field 'coordinates' is not valid")
 	}
 	lat, ok := coords[1].(float64)
 	if !ok {
-		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object %v field \"coordinates\" is not valid", v)
+		return Point{}, fmt.Errorf("pseudo-type GEOMETRY object field 'coordinates' is not valid")
 	}
 
 	return Point{
@@ -86,7 +182,7 @@ func UnmarshalPoint(v interface{}) (Point, error) {
 func UnmarshalLineString(v interface{}) (Line, error) {
 	points, ok := v.([]interface{})
 	if !ok {
-		return Line{}, fmt.Errorf("pseudo-type GEOMETRY object %v field \"coordinates\" is not valid", v)
+		return Line{}, fmt.Errorf("pseudo-type GEOMETRY object field 'coordinates' is not valid")
 	}
 
 	var err error
@@ -103,7 +199,7 @@ func UnmarshalLineString(v interface{}) (Line, error) {
 func UnmarshalPolygon(v interface{}) (Lines, error) {
 	lines, ok := v.([]interface{})
 	if !ok {
-		return Lines{}, fmt.Errorf("pseudo-type GEOMETRY object %v field \"coordinates\" is not valid", v)
+		return Lines{}, fmt.Errorf("pseudo-type GEOMETRY object field 'coordinates' is not valid")
 	}
 
 	var err error
