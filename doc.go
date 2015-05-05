a2ffb7dc21ccc6b485c027ebff33988935cc6181
Daniel Cannon <daniel@danielcannon.co.uk>
2015-05-05 23:03:06 +0100

    Bump version number + updated changelog

--- doc.go
+++ doc.go
@@ -1,6 +1,6 @@
 // Package gorethink implements a Go driver for RethinkDB
 //
-// Current version: v0.7.1 (RethinkDB v2.0)
+// Current version: v0.7.2 (RethinkDB v2.0)
 // For more in depth information on how to use RethinkDB check out the API docs
 // at http://rethinkdb.com/api
 package gorethink
