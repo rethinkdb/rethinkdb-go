e1bc42d7b6ea0e5d92ad7bf9a05d254a6854ff8e
Daniel Cannon <daniel@danielcannon.co.uk>
2016-03-11 22:29:54 +0000

    Revert "Merge branch 'error-codes' into develop"
    This reverts commit 018a7bcd63c3b6320db6cfcb2dddb15e2cff195d, reversing
    changes made to d577e218cfb84b85514342eece4756d7fe3e2355.

--- connection.go
+++ connection.go
@@ -26,7 +26,6 @@ type Response struct {
 	Responses []json.RawMessage         `json:"r"`
 	Backtrace []interface{}             `json:"b"`
 	Profile   interface{}               `json:"p"`
-	ErrorType p.Response_ErrorType      `json:"e"`
 }
 
 // Connection is a connection to a rethinkdb database. Connection is not thread
